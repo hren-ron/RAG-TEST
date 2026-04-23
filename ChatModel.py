2653785a5a9eb8e7f9e4ef4341a8235607ebf393
hren <hren_ron@hotmail.com>
2026-04-24 03:41:25 +0800

    更新 ChatModel.py

--- ChatModel.py
+++ ChatModel.py
@@ -7,8 +7,10 @@ from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 model = ChatTongyi(model="qwen3-max")
 
 messages = [
-    SystemMessage(content="你是一个边塞诗人"),
-    HumanMessage(content="写一首唐诗")
+    # SystemMessage(content="你是一个边塞诗人"),
+    ('system', "你是一个边塞诗人"),
+    ('human', "你写一首唐诗")
+    #HumanMessage(content="写一首唐诗")
 ]
 
 res = model.stream(input=messages)
