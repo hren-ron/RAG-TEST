a9b600168fb18b63b14493e6a7f59bc71c87a869
hren <hren_ron@hotmail.com>
2026-04-24 03:33:12 +0800

    更新 ChatModel.py

---
+++ ChatModel.py
@@ -0,0 +1,17 @@
+
+
+from langchain_community.chat_models.tongyi import ChatTongyi
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+
+
+model = ChatTongyi(model="qwen3-max")
+
+messages = [
+    SystemMessage(content="你是一个边塞诗人"),
+    HumanMessage(content="写一首唐诗")
+]
+
+res = model.stream(input=messages)
+
+for chunk in res:
+    print(chunk.content, end=" ", flush=True)
